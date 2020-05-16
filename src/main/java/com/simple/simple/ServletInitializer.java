351410d80dae62ded9aa8dab696b94cff793fa1c
Manish Dubey <manish.dubey01@ad.infosys.com>
2020-05-17 00:02:32 +0200

    correctio

--- src/main/java/com/simple/simple/ServletInitializer.java
+++ src/main/java/com/simple/simple/ServletInitializer.java
@@ -3,8 +3,6 @@ package com.simple.simple;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-import com.simple.simple.*;
-
 public class ServletInitializer extends SpringBootServletInitializer {
 	
 	@Override
